3a75d34f1af44d13c8778a191ab7ba03c4f17e0d
Peter Törnstrand <peter@happiness.se>
2025-06-06 09:15:32 +0200

    Update formatting.

--- codebase_copy_ticket_reference.user.js
+++ codebase_copy_ticket_reference.user.js
@@ -2,14 +2,14 @@
 // @name     	Codebase: Copy ticket reference.
 // @version  	1
 // @grant    	none
-// @match			https://code.happiness.se/projects/*/tickets/*
-// @match 		https://happiness.codebasehq.com/projects/*/tickets/*
+// @match	https://code.happiness.se/projects/*/tickets/*
+// @match 	https://happiness.codebasehq.com/projects/*/tickets/*
 // ==/UserScript==
 
 /**
  * Add stylesheet to page.
  */
-function GM_addStyle(aCss) {
+function addStyle(aCss) {
   'use strict';
   let head = document.getElementsByTagName('head')[0];
   if (head) {
@@ -22,16 +22,14 @@ function GM_addStyle(aCss) {
   return null;
 };
 
-
-GM_addStyle(`
-	.Thread__subject { display: flex; gap: 8px; align-items: center; }
-	.Thread__subject .TicketId { display: inline-block; font-size: 75%; padding: 0 8px; border-radius: 4px; }
-  .CopyButton { cursor: pointer; border: 0; width: 20px; height: 20px; background: url('data:image/svg+xml,<svg viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g stroke="%23555" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path clip-rule="evenodd" d="M17.676 14.248a2.928 2.928 0 0 1-2.928 2.928h-7.32A2.928 2.928 0 0 1 4.5 14.248v-7.32A2.928 2.928 0 0 1 7.428 4h7.32a2.928 2.928 0 0 1 2.928 2.928v7.32Z"/><path d="M10.252 20h7.32a2.928 2.928 0 0 0 2.928-2.928v-7.32"/></g></svg>'); }
+addStyle(`
+.Thread__subject { display: flex; gap: 8px; align-items: center; }
+.Thread__subject .TicketId { display: inline-block; font-size: 75%; padding: 0 8px; border-radius: 4px; }
+.CopyButton { cursor: pointer; border: 0; width: 20px; height: 20px; background: url('data:image/svg+xml,<svg viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g stroke="%23555" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path clip-rule="evenodd" d="M17.676 14.248a2.928 2.928 0 0 1-2.928 2.928h-7.32A2.928 2.928 0 0 1 4.5 14.248v-7.32A2.928 2.928 0 0 1 7.428 4h7.32a2.928 2.928 0 0 1 2.928 2.928v7.32Z"/><path d="M10.252 20h7.32a2.928 2.928 0 0 0 2.928-2.928v-7.32"/></g></svg>'); }
 `);
 
 // Get ticket status.
 const classList = document.querySelector('.js-ticket-property .TicketProperties__tag').classList;
-
 
 const url = new URL(document.URL);
 const path = url.pathname;
