333cb893b99ed9d2009b9102163d801a162b67af
Peter Törnstrand <peter@happiness.se>
2025-06-06 09:48:52 +0200

    Add new todo.

--- codebase_ticket_improvements.user.js
+++ codebase_ticket_improvements.user.js
@@ -6,9 +6,12 @@
 // @match		https://happiness.codebasehq.com/projects/*/tickets/*
 // ==/UserScript==
 
-// @todo Move .js-ticket-properties to container .right (remember to scope new CSS not to affect properties at the bottom of a ticket)
+// @todo Move .js-ticket-properties to container .right (remember to scope new CSS not to affect properties
+//   at the bottom of a ticket)
 // @todo Move .ThreadMeta to container .right, after .js-ticket-properties
 // @todo Move information from "Related milestone" box (.box box--sidebar) into .js-ticket-properties
 // @todo Change box with info "This ticket can be viewed by anyone..." into an icon (lock open, locked closed)
 //   display to the right of the ticket title or perhaps part of .js-ticket-properties
 // @todo Move the ticket update avatar to the left of the update and make it bigger (48x48)
+// @todo Add the "Related ticket" functionality to the sidebar. If there are no related tickets just display
+//   the button "Mark a ticket as blocking this ticket" but change the text to "Add sub-ticket".
