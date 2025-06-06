5b5e4f515fc1c00c3d99c9a2602936b2522c669f
Peter Törnstrand <peter@happiness.se>
2025-06-06 10:15:37 +0200

    Create codebase_global.user.js

---
+++ codebase_global.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name	Codebase: Global improvements
+// @version	1
+// @grant	none
+// @match	https://code.happiness.se/*
+// @match	https://happiness.codebasehq.com/*
+// ==/UserScript==
+
+// @todo Change the header background color to #150547 (same as DeployHQ)
+// @todo Change the .site-header__organisation to instead display the company logo.
