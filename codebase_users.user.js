ea3a3107394e396563900ae5d99a07a058c79213
Peter Törnstrand <peter@happiness.se>
2025-06-06 10:14:25 +0200

    Create codebase_users.user.js

---
+++ codebase_users.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name	Codebase: Users improvements
+// @version	1
+// @grant	none
+// @match	https://code.happiness.se/users/*
+// @match	https://happiness.codebasehq.com/users/*
+// ==/UserScript==
+
+// @todo Add a summary on the users/<X> page of all the tickets user has worked on today grouped
+//   by project for easy copy and paste into Harvest.
