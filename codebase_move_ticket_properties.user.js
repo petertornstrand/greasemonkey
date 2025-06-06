56241d65fb237ae66079e4e9145a63efff6dc7c7
Peter Törnstrand <peter@happiness.se>
2025-06-06 09:31:46 +0200

    Scaffold for new script.

---
+++ codebase_move_ticket_properties.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name		Codebase: Move ticket properties
+// @version	1
+// @grant		none
+// @match		https://code.happiness.se/projects/*/tickets/*
+// @match		https://happiness.codebasehq.com/projects/*/tickets/*
+// ==/UserScript==
