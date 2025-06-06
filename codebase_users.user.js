251bcca8a5bffd6978ea067af17e9321b4c6adaf
Peter Törnstrand <peter@happiness.se>
2025-06-06 10:52:09 +0200

    Add namespace and require.

--- codebase_users.user.js
+++ codebase_users.user.js
@@ -1,10 +1,22 @@
 // ==UserScript==
-// @name	Codebase: Users improvements
-// @version	1
-// @grant	none
-// @match	https://code.happiness.se/users/*
-// @match	https://happiness.codebasehq.com/users/*
+// @name       Codebase: Users improvements
+// @namespace  https://www.happiness.se
+// @require    https://raw.githubusercontent.com/petertornstrand/greasemonkey/refs/heads/main/codebase_common.js
+// @version    1
+// @grant      none
+// @match      https://code.happiness.se/users/*
+// @match      https://happiness.codebasehq.com/users/*
 // ==/UserScript==
 
 // @todo Add a summary on the users/<X> page of all the tickets user has worked on today grouped
 //   by project for easy copy and paste into Harvest.
+
+/**
+ * Entry point for script.
+ */
+async function main() {
+  // Code goes here...
+}
+
+// Runt it.
+main();
